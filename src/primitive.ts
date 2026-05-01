1575a3fe60a1b50e183af733e324a02d0c583f06
islamsorifulhero <islamsoriful.hero@gmail.com>
2026-05-01 22:01:06 +0600

    Primitive data type: Javascript vs TypeScript

---
+++ src/primitive.ts
@@ -0,0 +1,16 @@
+// JavaScript to TypeScript Convert
+// string , number, boolean, undefined, null
+
+// Ts: never, unknown, void
+
+let userName: string = 'soriful123';
+
+let userId = 123;
+
+// userName.toFixed();
+userId.toFixed;
+
+let isAdmin: boolean = false;
+isAdmin = true;
+
+let x: undefined = undefined;
